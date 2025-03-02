9d15444e114914bb9c3da2cbdb5e7d93b4f48e9e
Waris Warit <st124946@ait.ac.th>
2025-03-02 18:04:27 +0700

    commit

--- app/app.py
+++ app/app.py
@@ -1,46 +1,42 @@
 from flask import Flask, render_template, request
-# import spacy
-import pdfplumber
-from parser import ResumeParser
+from sentence_transformers import SentenceTransformer, util
+import pandas as pd
+import numpy as np
+# Load the pre-trained sentence transformer model
+model = SentenceTransformer('all-MiniLM-L6-v2')
 
 app = Flask(__name__)
-# nlp = spacy.load("en_core_web_sm")
 
-@app.route('/')
+def get_corpus():
+    corpus = []
+    parquet_file = "../data/0000.parquet"
+    df = pd.read_parquet(parquet_file, engine='pyarrow')
+    for idx,rows in df.iterrows():
+        if idx > 20:
+            break
+        for sent in rows['set']:
+           corpus.append(sent)
+    return corpus
+
+# Define a function to calculate cosine similarity
+def get_cosine_similarity(query, corpus):
+  query_embedding = model.encode(query)
+  corpus_embeddings = model.encode(corpus)
+  return util.cos_sim(query_embedding, corpus_embeddings).tolist()[0]
+
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template('index.html')
+  if request.method == "POST":
+    query = request.form["query"]
+    corpus = get_corpus()
+    # corpus = ["This is a sample sentence.", "This sentence is similar to the first one.", "This one is a bit different."]
+    similarity = get_cosine_similarity(query, corpus)
+    index_max = np.argmin(similarity)
+    max_sim = np.max(similarity)
+    return render_template("index.html", query=query, similarity={"similarity":max_sim, "most_similar_sent":corpus[index_max]})
+    
+  else:
+    return render_template("index.html")
 
-@app.route('/upload', methods=['POST'])
-def upload():
-    if 'resume' not in request.files:
-        return render_template('index.html', error='No file part')
-
-    resume_file = request.files['resume']
-
-    if resume_file.filename == '':
-        return render_template('index.html', error='No selected file')
-
-    if resume_file and allowed_file(resume_file.filename):
-        # text = extract_text_from_pdf(resume_file)
-        resume_details = parse_resume(resume_file)
-        return render_template('result.html', resume_details=resume_details)
-
-    return render_template('index.html', error='Invalid file format')
-
-def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() == 'pdf'
-
-# def extract_text_from_pdf(file):
-#     with pdfplumber.open(file) as pdf:
-#         text = ''
-#         for page in pdf.pages:
-#             text += page.extract_text()
-#     return text
-
-def parse_resume(file):
-   parser = ResumeParser(file)
-   parsed_result = parser.parse()
-   return parsed_result
-
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+  app.run(debug=True)
